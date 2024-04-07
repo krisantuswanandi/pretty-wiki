59328aa4ca0cc5e76738f2ba95fde999e6d327df
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-04-07 16:41:20 +0700

    show dev mode url

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -2,14 +2,17 @@ import { watch } from "fs";
 import path from "path";
 import { build } from "./build";
 
+const PORT = 3000;
 const srcDir = path.join(import.meta.dir, "../src");
 
 watch(srcDir, { recursive: true }, build);
 
 Bun.serve({
-  port: 3000,
+  port: PORT,
   fetch() {
     const file = Bun.file("dist/script.js");
     return new Response(file);
   },
 });
+
+console.log(`URL: http://localhost:${PORT}/script.user.js`);
